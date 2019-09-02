4cf803233318f283cb22a1a67ea06cfdd3c8703f
BlackCarDriver <13169346002@163.com>
2019-09-02 16:12:23 +0800

    change config

--- angular/src/app/server.service.ts
+++ angular/src/app/server.service.ts
@@ -10,7 +10,7 @@ export class ServerService {
 
   //important config !!!
 //本地开发配置
- private addr: string  = "/server"
+ private addr: string  = "/market"
  //服务器配置
   // private addr: string  = "https://www.blackcardriver.cn/server"
   constructor( 
---
+++ run-jenkins.sh
@@ -0,0 +1,31 @@
+# ng build 
+docker run \
+--rm --name ngbuild \
+-v /home/ubuntu/DockerWorkPlace/jenkins/jenkins_home/workspace/market/angular:/workplace \
+-w /workplace \
+blackcardriver2/mcis:ngcli \
+sh -c 'ls && pwd && ng build --prod --aot --base-href /market/'
+
+if [ $? -ne 0 ]; then exit 1; fi
+
+# copy build result
+if [ -w /nginx_home/market ]; then rm -rf /nginx_home/market && echo 'rm market old dist succeed!'; fi
+cp -rf angular/dist/mywebsite /nginx_home/market
+
+# go build 
+docker run \
+--rm --name gobuild \
+-v /home/ubuntu/DockerWorkPlace/jenkins/jenkins_home/workspace/market/Go/src:/go/src \
+golang:alpine \
+sh -c 'cd /go/src && go build TaobaoServer/main.go'
+
+if [ $? -ne 0 ]; then exit 2; fi
+
+# go run 
+docker run \
+--rm -d \
+--name market-setver \
+-v $PWD:/workplace \
+-p 4749:4747 \
+alpine:latest \
+sh -c 'cp /workplace/main /workplace/TaobaoServer/main && cd /workplace/TaobaoServer && ./main'
